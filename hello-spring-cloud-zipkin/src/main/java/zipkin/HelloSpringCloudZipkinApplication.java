ae7927386335b0968aad88291826fbe32d7bd360
lsf_2 <1813604115@qq.com>
2019-07-19 17:44:09 +0800

    123123123commitsssahsdhahsasdas

--- hello-spring-cloud-zipkin/src/main/java/zipkin/HelloSpringCloudZipkinApplication.java
+++ hello-spring-cloud-zipkin/src/main/java/zipkin/HelloSpringCloudZipkinApplication.java
@@ -2,8 +2,15 @@ package zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+/**
+ * zipkin 链路追踪
+ */
+@EnableZipkinServer
+@EnableDiscoveryClient
 public class HelloSpringCloudZipkinApplication {
 
     public static void main(String[] args) {
