ccfa213842ea5924d96f8dfc3fd956594b0be858
cdjjzz <1813604115@qq.com>
2019-07-21 14:41:20 +0800

    123asdasdasdasdasd sad

--- hello-spring-cloud-zipkin/src/main/java/zipkin/HelloSpringCloudZipkinApplication.java
+++ hello-spring-cloud-zipkin/src/main/java/zipkin/HelloSpringCloudZipkinApplication.java
@@ -3,13 +3,12 @@ package zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 /**
  * zipkin 链路追踪
  */
-@EnableZipkinServer
+//@EnableZipkinServer
 @EnableDiscoveryClient
 public class HelloSpringCloudZipkinApplication {
 
